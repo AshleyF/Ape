b3b599c9312a84445728bc5f60f224cf8dac09c7
Ashley Nathan Feniello <ashleyf@microsoft.com>
2012-12-31 08:58:59 -0800

    Added prelude

--- Ape.fs
+++ Ape.fs
@@ -6,7 +6,7 @@ type Syntax =
 
 let eval dict stack code =
     let rec eval' state = // function
-        printfn "STATE: %A" state
+        //printfn "STATE: %A" state
         match state with
         | dict, Quote q :: v :: stack', Word "cons" :: code' -> eval' (dict, Quote (v :: q) :: stack', code')
         | dict, Quote (v :: q) :: stack', Word "snoc" :: code' -> eval' (dict, Quote q :: v :: stack', code')
@@ -44,31 +44,51 @@ let print code =
         | [] -> let len = output.Length in if len = 0 then "" else output.Substring(0, len - 1)
     print' "" code
 
+let prelude = "
+[_ let]                       drop  let
+[a let a]                     apply let
+[[] cons]                     quote let
+[quote a let a a]             dup   let
+[a let quote b let a b]       dip   let
+[quote a let quote e let a e] swap  let
+[snoc drop]                   head  let
+[snoc swap drop]              tail  let
+[[#t swap] dip eq]            if    let
+[#f #t if]                    not?  let
+"
+
+let dictionary, _, _ = prelude |> lex |> parse |> eval Map.empty []
+
 let test code expected =
-    let _, stack, _ = code |> lex |> parse |> eval Map.empty []
+    let _, stack, _ = code |> lex |> parse |> eval dictionary []
     let result = List.rev stack |> print
     if result <> expected then printfn "FAIL: %A <> %A" result expected
 
 test "a [] cons"      "[a]"
 test "a [b c] cons"   "[a b c]"
 test "[a] [b c] cons" "[[a] b c]"
-
 test "[a] snoc"       "a []"
 test "[a b c] snoc"   "a [b c]"
 test "[[a] b c] snoc" "[a] [b c]"
-
 test "foo foo yes no eq" "yes"
 test "foo bar yes no eq" "no"
 test "[foo bar [baz]] [foo bar [baz]] yes no eq" "yes"
-
 test "2.71 e let e" "2.71"
 test "[cons cons cons] cons3 let a b c [] cons3" "[a b c]"
+test "123 456 drop" "123"
+test "123 456 [drop] apply" "123"
+test "123 quote" "[123]"
+test "123 dup" "123 123"
+test "123 456 [quote] dip" "[123] 456"
+test "123 456 swap" "456 123"
+test "[a b c] head" "a"
+test "[a b c] tail" "[b c]"
 
 let rec repl (dict, stack, code) () =
     try
         let (dict, stack, _) as state' = Console.ReadLine() |> lex |> parse |> eval dict stack
-        stack |> print |> printf "%s\n>" |> repl state'
+        List.rev stack |> print |> printf "%s\n>" |> repl state'
     with ex -> printf "ERROR: %s" ex.Message |> repl (dict, stack, code)
 
 printf "Welcome to Ape\n>"
-repl (Map.empty, [], []) ()
+repl (dictionary, [], []) ()
